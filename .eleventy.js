de1398f665f3f987fd1a97a4c974ac7a1b3e273d
Denis Shevchenko <DAlShevchenko@sberbank.ru>
2021-10-25 22:38:56 +0300

    Added footer and header

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const markdown = require('markdown-it')({ html: true })
 module.exports = function (config) {
 	config.addPassthroughCopy('src/styles')
 	config.addPassthroughCopy('src/fonts')
+	config.addPassthroughCopy('src/images')
 
 	config.addPairedShortcode('markdown', (content) => {
 		return markdown.render(content)
@@ -13,6 +14,7 @@ module.exports = function (config) {
 			input: 'src',
 			output: 'dist',
 			layouts: 'layouts',
+			includes: 'includes',
 		},
 		dataTemplateEngine: 'njk',
 		markdownTemplateEngine: 'njk',
