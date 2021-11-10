b7a8fc6a0858b1e9f24fe576210b6e019b565de2
Denis Shevchenko <DAlShevchenko@sberbank.ru>
2021-11-10 17:43:33 +0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,6 @@ const presetEnv = require('postcss-preset-env')
 const nested = require('postcss-nested')
 const csso = require('postcss-csso')
 
-// Styles
-
 gulp.task('styles:compress', () => {
 	const plugins = [postImport(), presetEnv(), nested(), csso()]
 
@@ -17,18 +15,6 @@ gulp.task('styles:compress', () => {
 		.src('src/styles/style.css')
 		.pipe(postcss(plugins))
 		.pipe(gulp.dest('dist/styles/'))
-})
-
-gulp.task('styles:inline', () => {
-	return gulp
-		.src('dist/**/*.html')
-		.pipe(
-			replace(/<link rel="stylesheet" href="\/styles\/style.css">/, () => {
-				const style = fs.readFileSync('dist/style.css', 'utf8')
-				return '<style>' + style + '</style>'
-			})
-		)
-		.pipe(gulp.dest('dist'))
 })
 
 gulp.task('clean', () => {
